cc8a630093afe1c1c59b34addb2d5dd0dbcf8ae8
shandawei <shandawei@me.com>
2022-07-21 03:00:41 +0800

    修复项目名称的bug

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-app'`, () => {
+  it(`should have as title 'yzb-oapp-quick-start-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('yzb-oapp-quick-start-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('yzb-oapp-quick-start-angular app is running!');
   });
 });
